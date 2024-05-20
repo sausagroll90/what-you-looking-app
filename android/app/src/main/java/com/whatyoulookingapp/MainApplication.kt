862006b182d1c70e1b38ded727734b918689a817
Louisa Whitton <nowlouisa@gmail.com>
2024-05-20 09:47:43 +0100

    build: set up ViroReact

--- android/app/src/main/java/com/whatyoulookingapp/MainApplication.kt
+++ android/app/src/main/java/com/whatyoulookingapp/MainApplication.kt
@@ -10,6 +10,8 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.viromedia.bridge.ReactViroPackage
+
 
 class MainApplication : Application(), ReactApplication {
 
@@ -19,6 +21,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+              add(ReactViroPackage(ReactViroPackage.ViroPlatform.valueOf("AR")))
             }
 
         override fun getJSMainModuleName(): String = "index"
