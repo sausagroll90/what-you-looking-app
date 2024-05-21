fc6f637a24fa72c746486c9b9bdc6bbb7c9d711b
Dominic Francis <dominicf09@gmail.com>
2024-05-21 10:49:16 +0100

    feat: create function to transform coordinates for AR using user and target latitude and longitude and compass heading
    Add unit tests for util functions.

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,7 +1,7 @@
-import { getRelativePosition } from '../src/modules/utils';
+import { getPositionForAR, getRelativePosition } from '../src/modules/utils';
 
 describe('getRelativePosition', () => {
-  it('returns correct relative position for given user location and target', () => {
+  it('returns relative position for given user location and target', () => {
     const testTargetCoords = { latitude: 53.8, longitude: -1.55 };
     const testUserCoords = { coords: { latitude: 53.797, longitude: -1.557 } };
 
@@ -12,5 +12,33 @@ describe('getRelativePosition', () => {
 
     expect(relativePosition.x).toBeCloseTo(460, 0);
     expect(relativePosition.z).toBeCloseTo(-334, 0);
+  });
+});
+
+describe('getPositionForAR', () => {
+  it('returns transformed coordinates', () => {
+    const testUserCoords = { coords: { latitude: 53.797, longitude: -1.557 } };
+    const testTargetCoords = { latitude: 53.8, longitude: -1.55 };
+    const testCompassHeading = 45;
+
+    const transformedCoordinates = getPositionForAR(
+      testUserCoords,
+      testTargetCoords,
+      testCompassHeading,
+    );
+
+    expect(transformedCoordinates[0]).toBeCloseTo(89, 0);
+    expect(transformedCoordinates[1]).toBe(0);
+    expect(transformedCoordinates[2]).toBeCloseTo(-561, 0);
+  });
+
+  it('raises an expection when userPosition is null', () => {
+    const testUserCoords = null;
+    const testTargetCoords = { latitude: 53.8, longitude: -1.55 };
+    const testCompassHeading = 45;
+
+    expect(() =>
+      getPositionForAR(testUserCoords, testTargetCoords, testCompassHeading),
+    ).toThrow('user location not found');
   });
 });
--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -3,16 +3,13 @@ import { GeoPosition } from 'react-native-geolocation-service';
 const DEGREES_TO_RADIANS_CONVERSION = (2 * Math.PI) / 360;
 
 export function getRelativePosition(
-  userLocation: GeoPosition | null,
+  userLocation: GeoPosition,
   target: {
     latitude: number;
     longitude: number;
+    [key: string]: any;
   },
 ): { x: number; z: number } {
-  if (!userLocation) {
-    throw new Error('user location not found');
-  }
-
   const RADIUS_OF_EARTH_IN_METRES = 6_371_000;
 
   const relativeLatitude = userLocation.coords.latitude - target.latitude;
@@ -32,4 +29,25 @@ export function getRelativePosition(
     latitudeScaleFactor;
 
   return { x: relativeX, z: relativeZ };
+}
+
+export function getPositionForAR(
+  userLocation: GeoPosition | null,
+  target: { latitude: number; longitude: number; [key: string]: any },
+  compassHeading: number,
+): [x: number, y: number, z: number] {
+  if (!userLocation) {
+    throw new Error('user location not found');
+  }
+
+  const relativePosition = getRelativePosition(userLocation, target);
+  const headingInRadians = compassHeading * DEGREES_TO_RADIANS_CONVERSION;
+
+  return [
+    relativePosition.x * Math.cos(headingInRadians) +
+      relativePosition.z * Math.sin(headingInRadians),
+    0,
+    -relativePosition.x * Math.sin(headingInRadians) +
+      relativePosition.z * Math.cos(headingInRadians),
+  ];
 }
