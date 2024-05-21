d73f164c126360e5b8ef2ded395192ea941d8e42
AvrilHunter <avrilhunter15@gmail.com>
2024-05-21 16:02:44 +0100

    style: add styling to details page
    refactor the rendering if details are available

--- src/components/PlaceDetails.tsx
+++ src/components/PlaceDetails.tsx
@@ -1,18 +1,59 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Linking } from 'react-native';
 import { Props } from '../types/route';
 
 export default function PlaceDetails({ route }: Props): React.JSX.Element {
-  const { formattedAddress, name, rating, weekday_text, website } =
+  const { formattedAddress, name, rating, weekday_text, website, place_id } =
     route.params.placeData;
 
+  const handlePress = () => {
+    Linking.openURL('website');
+  };
+
   return (
-    <View>
-      <Text>{name}</Text>
-      <Text>{formattedAddress}</Text>
-      <Text>{rating} rating</Text>
-      <Text>{weekday_text}</Text>
-      <Text>{website}</Text>
+    <View style={styles.container}>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.data}>{formattedAddress}</Text>
+      <Text style={styles.data}>Opening Hours:</Text>
+      {weekday_text &&
+        weekday_text.map((day) => {
+          return (
+            <Text key={day} style={styles.openingData}>
+              {day}
+            </Text>
+          );
+        })}
+      {rating && <Text style={styles.data}>{rating} rating</Text>}
+      {website && (
+        <View style={styles.button}>
+          <Button title="Website" onPress={handlePress} />
+        </View>
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+  },
+  name: {
+    fontWeight: 'bold',
+    color: '#136f63',
+    fontSize: 32,
+  },
+  data: {
+    color: '#032b43',
+    fontSize: 16,
+    padding: 15,
+  },
+  openingData: {
+    color: '#032b43',
+    fontSize: 16,
+  },
+  button: {
+    padding: 23,
+  },
+});
