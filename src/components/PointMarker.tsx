69d0a09e54acce3af3102c52ec1933f0ff57ab93
AkivaKn <akivakaufman@gmail.com>
2024-05-21 10:44:55 +0100

    feat: add building icon and wrap in viro box

--- src/components/PointMarker.tsx
+++ src/components/PointMarker.tsx
@@ -1,22 +1,50 @@
-import { ViroImage } from '@viro-community/react-viro';
+import {
+  ViroAnimations,
+  ViroBox,
+  ViroMaterials,
+} from '@viro-community/react-viro';
 import React from 'react';
 
 export default function PointMarker(props: {
   location: {
     geometry: { location: { lat: number; lng: number } };
     types: string[];
+    name: string;
   };
 }) {
   const imageSrc =
     props.location.types[0] === 'museum'
       ? require('../../res/icons/museum.png')
-      : require('../../res/icons/cafe.png');
+      : props.location.types[0] === 'cafe'
+      ? require('../../res/icons/cafe.png')
+      : require('../../res/icons/building.png');
+
+  ViroMaterials.createMaterials({
+    building: {
+      diffuseTexture: imageSrc,
+    },
+  });
+
+  ViroAnimations.registerAnimations({
+    rotate: {
+      properties: {
+        rotateY: '+=180',
+      },
+      duration: 5000,
+    },
+  });
+
+  const handleClick = () => {
+    console.log('clicked');
+  };
 
   return (
-    <ViroImage
-      source={imageSrc}
-      position={[0, 0, -2]}
+    <ViroBox
+      materials={['building']}
       scale={[0.3, 0.3, 0.3]}
+      position={[0, 0, -1]}
+      animation={{ name: 'rotate', run: true, loop: true }}
+      onClick={handleClick}
     />
   );
 }
