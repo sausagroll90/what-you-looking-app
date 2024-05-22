1055bf54e3548e466fc970937067114533782efa
AvrilHunter <avrilhunter15@gmail.com>
2024-05-22 15:28:52 +0100

    fix: remove header to avoid app closing

--- App.tsx
+++ App.tsx
@@ -3,31 +3,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/components/HomeScreen';
 import PlaceDetails from './src/components/PlaceDetails';
-import PointMarker from './src/components/PointMarker';
-import { Button } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={'Home'}>
-        {/* {error === 'location' ? (
-          <Stack.Screen name="Error" component={LocationDeniedError} />
-        ) : ( */}
-        <>
-          <Stack.Screen
-            name="PlaceDetails"
-            component={PlaceDetails}
-            options={{
-              headerRight: () => {
-                <Button title="test" />;
-              },
-            }}
-          />
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </>
-        {/* )} */}
+      <Stack.Navigator initialRouteName={'PlaceDetails'}>
+        <Stack.Screen
+          name="PlaceDetails"
+          component={PlaceDetails}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
